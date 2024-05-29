b137408e0ea2a80c910f0be66e2af1be5828dcd1
feliperdamaceno <feliperdamaceno@sent.com>
2024-05-29 17:34:18 +0100

    fix: push footer to the bottom

--- src/routes/Root/index.tsx
+++ src/routes/Root/index.tsx
@@ -5,9 +5,9 @@ import Header from '@/components/header';
 
 const Root = () => {
   return (
-    <div className="container">
+    <div className="container flex flex-col min-h-screen">
       <Header />
-      <main className="p-20 space-y-8">
+      <main className="flex-1 p-20 space-y-8">
         <Outlet />
       </main>
       <Footer />
