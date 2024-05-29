5cbf8e6fc3ae424e1fd59916a7753f7e023084c4
Nicola Hearn <nfhearn@gmail.com>
2024-05-29 09:10:33 +0100

    deletes unnecessary imports in colorPicker

--- src/components/colorPicker/index.tsx
+++ src/components/colorPicker/index.tsx
@@ -1,16 +1,4 @@
-import { DividerHorizontalIcon } from '@radix-ui/react-icons';
-import {
-  Block,
-  Chrome,
-  Circle,
-  Colorful,
-  Compact,
-  Github,
-  Material,
-  Sketch,
-  Slider,
-  Wheel,
-} from '@uiw/react-color';
+import { Sketch } from '@uiw/react-color';
 
 const ColorPicker = () => {
   return (
