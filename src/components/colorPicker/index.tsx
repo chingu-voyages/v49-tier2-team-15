3edfce28a7e97e0e697e6e5d749d2508d747b427
Nicola Hearn <nfhearn@gmail.com>
2024-05-30 08:33:34 +0100

    has color picker and a color panel displaying the color that has been selected

--- src/components/colorPicker/index.tsx
+++ src/components/colorPicker/index.tsx
@@ -1,9 +1,26 @@
 import { Sketch } from '@uiw/react-color';
+import React, { useState } from 'react';
 
 const ColorPicker = () => {
+  const [currentColor, setCurrentColor] = useState('#39FF14');
+
+  // function handleColorchange(event) {
+  //   setColor(event.target.value);
+  // }
+
   return (
-    <div>
-      <Sketch />
+    <div className="color-picker-container flex justify-between w-1/2">
+      <Sketch color={currentColor} />
+      <div className="w-3/5">
+        <div
+          className="color-display h-32 justify-center content-center rounded-md"
+          style={{ backgroundColor: currentColor }}
+        >
+          <p className="text-center">Selected Color</p>
+        </div>
+        {/* <label htmlFor="SelectedColor">Select a Color</label> */}
+        <input type="text" value={currentColor} />
+      </div>
     </div>
   );
 };
