a6d548bbdc6a2c6d8e7994b59b278320ab51bf09
feliperdamaceno <feliperdamaceno@sent.com>
2024-06-02 15:23:30 +0100

    refactor: change footer links and signature

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,36 +6,20 @@ const Footer = () => {
           <div className="text-xl font-bold text-transparent bg-gradient-to-r from-teal-500 via-orange-500 to-violet-700 bg-clip-text">
             Color Inspo
           </div>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
         </div>
+
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024{' '}
-          <a href="https://colorinspo.com/" className="hover:underline">
-            Flowbite™
+          © {new Date().getFullYear()}{' '}
+          <a
+            className="hover:underline"
+            href="https://www.chingu.io/"
+            target="_blank"
+          >
+            Chingu
           </a>
-          . All Rights Reserved.
+          . Voyage 49 - Tier 02 - Team 15
         </span>
       </div>
     </footer>
